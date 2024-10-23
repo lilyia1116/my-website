7c6bc0aeefe2ff82664541cfd95b4ad6366bf969
lilyia1116 <963763280@qq.com>
2024-10-23 14:09:56 +0800

    Updated HTML file with corrections

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-// Toggle dark mode
-const darkModeToggle = document.getElementById('darkModeToggle');
+<script>
+    const toggleButton = document.createElement('button');
+    toggleButton.innerText = 'Toggle Dark Mode';
+    document.body.prepend(toggleButton);
 
-darkModeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-});
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        document.body.classList.toggle('light-mode');
+
+        if (document.body.classList.contains('dark-mode')) {
+            toggleButton.innerText = 'Toggle Light Mode';
+        } else {
+            toggleButton.innerText = 'Toggle Dark Mode';
+        }
+    });
+
+    // Set initial mode
+    document.body.classList.add('light-mode');
+</script>
