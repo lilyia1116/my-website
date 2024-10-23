7fab74cc37f47e700e87e247e171480133b25211
lilyia1116 <963763280@qq.com>
2024-10-23 08:08:09 +0000

    https://github.com/lilyia1116/my-website/tree/master

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 function toggleMode() {
-    const body = document.body;
-    body.style.backgroundColor = body.style.backgroundColor === 'black' ? 'white' : 'black';
-    body.style.color = body.style.color === 'white' ? 'black' : 'white';
+    document.body.classList.toggle('dark-mode');
 }
