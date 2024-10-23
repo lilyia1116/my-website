92c2230103a99bbcc3d993d6495ac1899a6a03e2
lilyia1116 <963763280@qq.com>
2024-10-23 07:52:55 +0000

    https://github.com/lilyia1116/my-website/tree/master

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-<script>
-    const toggleButton = document.createElement('button');
-    toggleButton.innerText = 'Toggle Dark Mode';
-    document.body.prepend(toggleButton);
-
-    toggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-        document.body.classList.toggle('light-mode');
-
-        if (document.body.classList.contains('dark-mode')) {
-            toggleButton.innerText = 'Toggle Light Mode';
-        } else {
-            toggleButton.innerText = 'Toggle Dark Mode';
-        }
-    });
-
-    // Set initial mode
-    document.body.classList.add('light-mode');
-</script>
+function toggleMode() {
+    const body = document.body;
+    body.style.backgroundColor = body.style.backgroundColor === 'black' ? 'white' : 'black';
+    body.style.color = body.style.color === 'white' ? 'black' : 'white';
+}
